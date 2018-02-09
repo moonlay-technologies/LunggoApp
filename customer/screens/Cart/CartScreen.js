fb13769e10e96ff6941d9a050c58b70bbcab2e66
Indera Aji Waskitho <indera.aji@travelmadezy.com>
2018-02-09 16:46:40 +0700

    replace cart loading animation

--- customer/screens/Cart/CartScreen.js
+++ customer/screens/Cart/CartScreen.js
@@ -9,11 +9,11 @@ import { getCart } from './CartController';
 
 export default class CartScreen extends React.Component {
 
-  constructor (props) {
+  constructor(props) {
     super(props);
     this.state = {
       isLoading: true,
-      list:[],
+      list: [],
     };
   }
 
@@ -22,23 +22,32 @@ export default class CartScreen extends React.Component {
   };
 
   componentDidMount() {
-    getCart().then( ({ cartId, list, totalPrice, status }) => {
-      this.setState({cartId, list, totalPrice, status, isLoading: false});
-      // this.forceUpdate();
-    }).catch(error=>console.log(error));
+    getCart().then(({ cartId, list, totalPrice, status }) => {
+      this.setState({ cartId, list, totalPrice, status, isLoading: false });
+    }).catch(error => console.log(error));
   }
 
   render() {
-    let {cartId, isLoading, list, totalPrice, status } = this.state;
-    let {props} = this;
+    let { cartId, isLoading, list, totalPrice, status } = this.state;
+    let { props } = this;
 
-    if (isLoading) return <ActivityIndicator size="large"/>
-    else if (status==200 && list && list.length > 0 ) return (
+    if (isLoading) return (
+      <View style={styles.container}>
+        <Image width="70" height="70" source={require('../assets/images/loader1.gif')} />
+      </View>)
+    else if (status == 200 && list && list.length > 0) return (
       <ListScreen list={list} totalPrice={totalPrice} cartId={cartId} {...props} />
     );
     else return <BlankScreen {...props} />
   }
 }
 
-// const styles = StyleSheet.create({
-// });
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    backgroundColor:'#fff',
+    alignItems:'center',
+    justifyContent:'center'
+  },
+});
+
