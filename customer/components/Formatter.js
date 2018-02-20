84355d393639cfcde1c9202da38b3fc53bec24a0
CarlosWirama <carloswirama@travelmadezy.com>
2018-02-20 18:15:05 +0700

    fix formatter merge bug

--- customer/components/Formatter.js
+++ customer/components/Formatter.js
@@ -19,11 +19,6 @@ export var number = int => int.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 //// Format price to "Rp 1.000.000"
 export var rupiah = int => 'Rp ' + number(int);
-export var price = rupiah;
+export var price = rupiah; //// use rupiah as default price
 
 // export var dollar = int => '$' + int;
-
-export var price = money => {
-	defaultMoneyFormat = rupiah; //// use rupiah as default price
-	return defaultMoneyFormat(money);
-}
