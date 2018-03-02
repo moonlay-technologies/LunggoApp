f9483d1d907e719d98068698b394980aab440365
Ali Zainal <ali@travelmadezy.com>
2018-03-02 16:03:04 +0700

    redesign beforeloginscreen

--- navigation/RootNavigation.js
+++ navigation/RootNavigation.js
@@ -53,8 +53,9 @@ export default class RootNavigator extends React.Component {
   rootStackNavigator = StackNavigator(
     {
       Main: {
+        screen:BeforeLoginScreen
         //screen: Dashboard
-        screen: MainTabNavigator
+        //screen: MainTabNavigator
         //screen: LoginScreen
         // screen: SubmitReview
         //screen: SubmitRating
